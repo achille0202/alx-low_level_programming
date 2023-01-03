4a4b62a08bbfc8a46a665c72d093e049ddc730c8
achille0202 <achilleayih@yahoo.com>
2023-01-03 15:09:26 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include"main.h"
 
-char word = '_putchar';
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	char school[10] = "_putchar";
+	char str[] = "_putchar";
+	int ch;
 
-	int i;
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
 
-	for (i = 0; i < 8; i++)
-		{
-			_putchar(school[i]);
-		}
-			_putchar(10);
-			return (0);
+	return (0);
 }
