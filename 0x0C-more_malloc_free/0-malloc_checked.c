3835968afc91a344990a7745ca3e619f8b044419
achille0202 <achilleayih@yahoo.com>
2023-01-13 02:52:24 -0800

    memloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,14 @@
-#include<stdlib>
+#include<stdlib.h>
 /** Prototype: void *malloc_checked(unsigned int b);
 * Returns a pointer to the allocated memory
 * if malloc fails, the malloc_checked function should cause normal process termination with a status value of 98
-**/
+*/
 
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b)
 { 
- int *my_mem;
- my_mem=malloc(sizeof(b))
- if (b == NULL)
+ void *my_mem;
+ my_mem=malloc(b);
+ if (my_mem == NULL)
 	exit(98);
-return(my_mem)
+return(my_mem);
 }
